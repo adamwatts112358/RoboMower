82edea21410d1a70ba3291cb34d95bf6ed882236
Adam Watts <39932368+adamwatts112358@users.noreply.github.com>
2021-04-10 19:59:52 -0500

    Update mower_controller.py
    Update code for controller, tank controls with two joysticks, and stop feature.

--- mower_controller.py
+++ mower_controller.py
@@ -1,22 +1,29 @@
 from evdev import list_devices, InputDevice, categorize, ecodes
 import RPi.GPIO as GPIO
 
+# To do:
+## Add button control to increase/decrease PWM freq.
+
 # Setup gamepad
+zeroL = 0.0
+zeroR = 0.0
 axis_max = 33000.0
+axis_tol = 10*330.0
 gamepad = InputDevice(list_devices()[0])
 print(gamepad)
 
 # Setup GPIO
 GPIO.setmode(GPIO.BCM)
-motorA_dir_pin = 17
-motorA_pwm_pin = 18
-motorB_dir_pin = 22
-motorB_pwm_pin = 23
+GPIO.setwarnings(False)
+motorA_dir_pin = 24 # Green
+motorA_pwm_pin = 23 # Blue
+motorB_dir_pin = 27 # Orange
+motorB_pwm_pin = 22 # White
 GPIO.setup(motorA_dir_pin, GPIO.OUT)
 GPIO.setup(motorA_pwm_pin, GPIO.OUT)
 GPIO.setup(motorB_dir_pin, GPIO.OUT)
 GPIO.setup(motorB_pwm_pin, GPIO.OUT)
-pwm_freq = 500
+pwm_freq = 500.0
 pwm_A = GPIO.PWM(motorA_pwm_pin, pwm_freq)
 pwm_B = GPIO.PWM(motorB_pwm_pin, pwm_freq)
 pwm_A.start(0)
@@ -27,37 +34,59 @@ def stop():
     GPIO.output(motorA_pwm_pin, GPIO.LOW)
     GPIO.output(motorB_dir_pin, GPIO.LOW)
     GPIO.output(motorB_pwm_pin, GPIO.LOW)
+    pwm_A.ChangeDutyCycle(0.0)
+    pwm_B.ChangeDutyCycle(0.0)
     
+
+def zero(event):
+    global zeroL, zeroR
+    if event.code == ecodes.ABS_Y:
+        # Zero the left stick
+        zeroL = event.value
+    elif event.code == ecodes.ABS_RY:
+        # Zero the right stick
+        zeroR = event.value
+
 def button(event):
     # B button
     if event.code == 305:
         if event.value == 1:
             print("Stop!")
             stop()
+            zero(event)
             
 def driveMotor(pwm_pin, dir_pin, value):
     # Set the direction pin
-    if value > 0.0:
-        GPIO.output(dir_pin, GPIO.HIGH)
+    if value <= 0.0:
         direction = 'forward'
-    elif value < 0.0:
-        GPIO.output(dir_pin, GPIO.LOW)
+        GPIO.output(dir_pin, GPIO.HIGH)
+    elif value > 0.0:
         direction = 'backward'
-        
+        GPIO.output(dir_pin, GPIO.LOW)
+
     # Set the PWM speed
-    speed = 100.0*(value/axis_max)
-    pwn_pin.ChangeDutyCycle(speed)
-    
-    print('%s %s %f'%(pwn_pin, direction, speed))
-    
+    speed = float(100.0*(abs(value)/axis_max))
+    pwm_pin.ChangeDutyCycle(speed)
+    print('%s %s %f'%(pwm_pin, direction, speed))
+
+
 # Main controller loop
-for event in gamepad.read_loop():
-    # Joystick
-    if event.type == ecodes.EV_ABS:
-        if event.code == ecodes.ABS_X: # Left stick Up/Down
-            driveMotor(motorA_pwm_pin, motorA_dir_pin, event.value)
-        elif event.code == ecodes.ABS_Y: # Right stick Up/Down
-            driveMotor(motorB_pwm_pin, motorB_dir_pin, event.value)
-    # Buttons
-    elif event.type == ecodes.EV_KEY:
-        button(event)
+try:
+    for event in gamepad.read_loop():
+        # Joystick
+        if event.type == ecodes.EV_ABS:
+            value = event.value
+            if abs(event.value) < axis_tol:
+                value = 0.0
+            if event.code == ecodes.ABS_Y: # Left stick Up/Down
+               driveMotor(pwm_A, motorA_dir_pin, value)
+            elif event.code == ecodes.ABS_RY: # Right stick Up/Down
+                driveMotor(pwm_B, motorB_dir_pin, value)
+        # Buttons
+        elif event.type == ecodes.EV_KEY:
+            button(event)
+except:
+    pass
+finally:
+    stop()
+    #GPIO.cleanup()
